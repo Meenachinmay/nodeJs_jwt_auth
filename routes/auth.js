4edcdd9141f734f68edd63703324d5031363b7dd
meenachinmay <chinmayanand896@gmail.com>
2020-07-26 18:24:28 +0530

    unhandled promise error resolved

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
 
 const { validateUserForSignUp, validateUserForLogin} = require('../validations/validations');
 
@@ -12,13 +14,21 @@ router.post('/register', async (req,res) => {
 
     if (UserValidationResponse.error) return res.status(400).json({
         success: false,
-        errors: UserValidationResponse.error.details[0].message
+        error: UserValidationResponse.error.details[0].message
     })
+
+    // Checking if the userEmail is already exist
+    const existedEmail = await User.findOne({ email: req.body.email })
+    if (existedEmail) return res.status(400).json({success: false, message: "Email is already in use", email: existedEmail.email });   
+
+    // HASHING PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
